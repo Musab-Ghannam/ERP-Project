8f5547f87da6d90b541aaa12a1796609d9ec2bee
Musab-Ghannam <115452464+Musab-Ghannam@users.noreply.github.com>
2023-02-14 10:54:41 -0800

    14 2

---
+++ University_Registration/University_Registration/Controllers/mainHomeController.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace University_Registration.Controllers
+{
+    public class mainHomeController : Controller
+    {
+        // GET: mainHome
+        public ActionResult Index()
+        {
+            return View();
+        }
+
+        public ActionResult About()
+        {
+            return View();
+        }
+
+        public ActionResult Contact()
+        {
+            return View();
+        }
+
+        public ActionResult coarses()
+        {
+            return View();
+        }
+    }
+}
